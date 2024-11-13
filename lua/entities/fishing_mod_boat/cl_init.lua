955628ea1cbb07a515bf2058615f116f2cedd93a
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2024-11-13 23:01:18 +0300

    Remove boat

--- lua/entities/fishing_mod_boat/cl_init.lua
+++
@@ -1,7 +0,0 @@
-language.Add("fishing_mod_boat","Fishing Boat")
-
-include("shared.lua")
-
-function ENT:Draw()
-	self:DrawModel()
-end
--- lua/entities/fishing_mod_boat/shared.lua
+++
@@ -1,5 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_anim"
-ENT.PrintName = "Лодка"
-ENT.Spawnable = true
-ENT.Category = "Рыбалка"
